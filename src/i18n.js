807eb946766ab5d460f361e4eef6c4222dc3fda6
Krishna Avtar <krishnaavtar955@gmail.com>
2025-10-01 17:52:15 +0530

    Implement language

---
+++ src/i18n.js
@@ -0,0 +1,38 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+
+// Import all translations
+import enTranslations from './locales/en/translation.json';
+import hiTranslations from './locales/hi/translation.json';
+import taTranslations from './locales/ta/translation.json';
+
+const resources = {
+  en: {
+    translation: enTranslations
+  },
+  hi: {
+    translation: hiTranslations
+  },
+  ta: {
+    translation: taTranslations
+  }
+};
+
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    resources,
+    fallbackLng: 'en',
+    debug: false,
+    interpolation: {
+      escapeValue: false, // not needed for react as it escapes by default
+    },
+    detection: {
+      order: ['localStorage', 'navigator'],
+      caches: ['localStorage'],
+    },
+  });
+
+export default i18n;
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,17 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { I18nextProvider } from 'react-i18next';
+import i18n from './i18n';
 import './index.css';
 import App from './App.jsx';
 import CustomThemeProvider from './components/ThemeProvider';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <CustomThemeProvider>
-      <App />
-    </CustomThemeProvider>
+    <I18nextProvider i18n={i18n}>
+      <CustomThemeProvider>
+        <App />
+      </CustomThemeProvider>
+    </I18nextProvider>
   </StrictMode>
 );
